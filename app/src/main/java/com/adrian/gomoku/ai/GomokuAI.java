661e8ce6c785884919d0b7f16012552298c53b8c
ranqing <rq880113@163.com>
2016-12-29 09:23:40 +0800

    add ai

--- app/src/main/java/com/adrian/gomoku/ai/GomokuAI.java
+++ app/src/main/java/com/adrian/gomoku/ai/GomokuAI.java
@@ -3,6 +3,8 @@ package com.adrian.gomoku.ai;
 import android.graphics.Point;
 import android.util.Log;
 
+import java.util.ArrayList;
+
 /**
  * Created by ranqing on 2016/12/27.
  */
@@ -16,13 +18,20 @@ public class GomokuAI {
     private boolean[][][] wins;
     private int[] playerWin;
     private int[] aiWin;
-    private int winCount = 572; //赢和可能种数
+    private int winCount = 572; //胜的可能种数
+
+    private int[][] playerScore;
+    private int[][] aiScore;
+    private int maxScore;
+    private int u, v;
 
     public GomokuAI(int maxLineCount) {
         this.maxLineCount = maxLineCount;
         wins = new boolean[maxLineCount][maxLineCount][winCount];
         playerWin = new int[winCount];
         aiWin = new int[winCount];
+        playerScore = new int[maxLineCount][maxLineCount];
+        aiScore = new int[maxLineCount][maxLineCount];
         initCount();
     }
 
@@ -104,5 +113,37 @@ public class GomokuAI {
             }
         }
         return false;
+    }
+
+    public Point getBestPoint(ArrayList whiteArray, ArrayList blackArray) {
+        for (int i = 0; i < maxLineCount; i++) {
+            for (int j = 0; j < maxLineCount; j++) {
+                Point p = new Point(i, j);
+                if (!(whiteArray.contains(p) || blackArray.contains(p))) { //查找空闲点
+                    for (int k = 0; k < winCount; k++) {
+                        if (wins[i][j][k]) {
+                            //遍历玩家赢法数组，给空闲点赋分（计算机拦截）
+                            if (playerWin[k] == 1) {
+                                playerScore[i][j] += 200;
+                            } else if (playerWin[k] == 2) {
+                                playerScore[i][j] += 400;
+                            } else if (playerWin[k] == 3) {
+                                playerScore[i][j] += 2000;
+                            } else if (playerWin[k] == 4) {
+                                playerScore[i][j] += 10000;
+                            }
+
+                            //遍历AI赢法数组，给空闲点赋分（计算机进攻）
+                            if (aiWin[k] == 1) {
+                                aiScore[i][j] += 220;
+                            } else if (aiWin[k] == 2) {
+                                aiScore[i][j] += 420;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return null;
     }
 }
