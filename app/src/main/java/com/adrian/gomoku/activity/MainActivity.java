af220fdc013b3fca444f1a7d86e626a78315052e
ranqing <rq880113@163.com>
2016-12-31 01:29:25 +0800

    add ai

--- app/src/main/java/com/adrian/gomoku/activity/MainActivity.java
+++ app/src/main/java/com/adrian/gomoku/activity/MainActivity.java
@@ -1,30 +1,36 @@
 package com.adrian.gomoku.activity;
 
-import android.content.DialogInterface;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Build;
-import android.support.v7.app.AlertDialog;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.adrian.gomoku.R;
-import com.adrian.gomoku.activity.BaseActivity;
-import com.adrian.gomoku.ai.GomokuAI;
-import com.adrian.gomoku.views.GomokuView;
+import com.yalantis.contextmenu.lib.ContextMenuDialogFragment;
+import com.yalantis.contextmenu.lib.MenuObject;
+import com.yalantis.contextmenu.lib.MenuParams;
+import com.yalantis.contextmenu.lib.interfaces.OnMenuItemClickListener;
+import com.yalantis.contextmenu.lib.interfaces.OnMenuItemLongClickListener;
 
-import de.cketti.library.changelog.ChangeLog;
+import java.util.ArrayList;
+import java.util.List;
 
-import static java.security.AccessController.getContext;
+public class MainActivity extends BaseActivity implements OnMenuItemClickListener, OnMenuItemLongClickListener {
 
-public class MainActivity extends BaseActivity implements GomokuView.IGameOverListener, View.OnClickListener {
-
-    private GomokuView mGomokuView;
-    private AlertDialog mAlertDialog;
-    private Button mRevokeBtn;
-    private Button mRestartBtn;
+    private FragmentManager fragmentManager;
+    private ContextMenuDialogFragment mMenuDialogFragment;
 
     private long mLastBackPress;
     private static final long mBackPressThreshold = 3500;
@@ -44,72 +50,150 @@ public class MainActivity extends BaseActivity implements GomokuView.IGameOverLi
     @Override
     protected void initViews() {
         setContentView(R.layout.activity_main);
-        mGomokuView = (GomokuView) findViewById(R.id.gomoku_view);
-        mRevokeBtn = (Button) findViewById(R.id.btn_revoke);
-        mRestartBtn = (Button) findViewById(R.id.btn_restart);
-        mGomokuView.setListener(this);
-        mRevokeBtn.setOnClickListener(this);
-        mRestartBtn.setOnClickListener(this);
-
-        ChangeLog cl = new ChangeLog(this);
-        if (cl.isFirstRun()) {
-            cl.getLogDialog().show();
-        }
-
-        mGomokuView.setAiOpened(true);
-        if (mGomokuView.isAiOpened()) {
-            mRevokeBtn.setVisibility(View.GONE);
-        } else {
-            mRevokeBtn.setVisibility(View.VISIBLE);
-        }
+        fragmentManager = getSupportFragmentManager();
+        initToolbar();
+        initMenuFragment();
+        addFragment(new MainFragment(), true, R.id.container);
     }
 
     @Override
     protected void loadData() {
     }
 
+    private void initMenuFragment() {
+        MenuParams menuParams = new MenuParams();
+        menuParams.setActionBarSize((int) getResources().getDimension(R.dimen.tool_bar_height));
+        menuParams.setMenuObjects(getMenuObjects());
+        menuParams.setClosableOutside(false);
+        mMenuDialogFragment = ContextMenuDialogFragment.newInstance(menuParams);
+        mMenuDialogFragment.setItemClickListener(this);
+        mMenuDialogFragment.setItemLongClickListener(this);
+    }
+
+    private List<MenuObject> getMenuObjects() {
+        // You can use any [resource, bitmap, drawable, color] as image:
+        // item.setResource(...)
+        // item.setBitmap(...)
+        // item.setDrawable(...)
+        // item.setColor(...)
+        // You can set image ScaleType:
+        // item.setScaleType(ScaleType.FIT_XY)
+        // You can use any [resource, drawable, color] as background:
+        // item.setBgResource(...)
+        // item.setBgDrawable(...)
+        // item.setBgColor(...)
+        // You can use any [color] as text color:
+        // item.setTextColor(...)
+        // You can set any [color] as divider color:
+        // item.setDividerColor(...)
+
+        List<MenuObject> menuObjects = new ArrayList<>();
+
+        MenuObject close = new MenuObject();
+        close.setResource(R.drawable.icn_close);
+
+        MenuObject send = new MenuObject(getString(R.string.theme_settings));
+        send.setResource(R.mipmap.theme);
+
+        MenuObject like = new MenuObject("Like profile");
+        Bitmap b = BitmapFactory.decodeResource(getResources(), R.drawable.icn_2);
+        like.setBitmap(b);
+
+        MenuObject addFr = new MenuObject("Add to friends");
+        BitmapDrawable bd = new BitmapDrawable(getResources(),
+                BitmapFactory.decodeResource(getResources(), R.drawable.icn_3));
+        addFr.setDrawable(bd);
+
+        MenuObject addFav = new MenuObject("Add to favorites");
+        addFav.setResource(R.drawable.icn_4);
+
+        MenuObject block = new MenuObject("Block user");
+        block.setResource(R.drawable.icn_5);
+
+        menuObjects.add(close);
+        menuObjects.add(send);
+        menuObjects.add(like);
+        menuObjects.add(addFr);
+        menuObjects.add(addFav);
+        menuObjects.add(block);
+        return menuObjects;
+    }
+
+    private void initToolbar() {
+        Toolbar mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        TextView mToolBarTextView = (TextView) findViewById(R.id.text_view_toolbar_title);
+        setSupportActionBar(mToolbar);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setHomeButtonEnabled(true);
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setDisplayShowTitleEnabled(false);
+        }
+        mToolbar.setNavigationIcon(R.drawable.btn_back);
+        mToolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+        mToolBarTextView.setText(R.string.app_name);
+    }
+
+    protected void addFragment(Fragment fragment, boolean addToBackStack, int containerId) {
+        invalidateOptionsMenu();
+        String backStackName = fragment.getClass().getName();
+        boolean fragmentPopped = fragmentManager.popBackStackImmediate(backStackName, 0);
+        if (!fragmentPopped) {
+            FragmentTransaction transaction = fragmentManager.beginTransaction();
+            transaction.add(containerId, fragment, backStackName)
+                    .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+            if (addToBackStack)
+                transaction.addToBackStack(backStackName);
+            transaction.commit();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(final Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.context_menu:
+                if (fragmentManager.findFragmentByTag(ContextMenuDialogFragment.TAG) == null) {
+                    mMenuDialogFragment.show(fragmentManager, ContextMenuDialogFragment.TAG);
+                }
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
     @Override
     public void onBackPressed() {
-        long currentTime = System.currentTimeMillis();
-        if (Math.abs(currentTime - mLastBackPress) > mBackPressThreshold) {
-            Toast.makeText(this, R.string.back_again_quit, Toast.LENGTH_SHORT).show();
-            mLastBackPress = currentTime;
+        if (mMenuDialogFragment != null && mMenuDialogFragment.isAdded()) {
+            mMenuDialogFragment.dismiss();
         } else {
-            super.onBackPressed();
+            long currentTime = System.currentTimeMillis();
+            if (Math.abs(currentTime - mLastBackPress) > mBackPressThreshold) {
+                Toast.makeText(this, R.string.back_again_quit, Toast.LENGTH_SHORT).show();
+                mLastBackPress = currentTime;
+            } else {
+                finish();
+            }
         }
     }
 
     @Override
-    public void gameOver(boolean isWhiteWin) {
-        int msgId = isWhiteWin ? R.string.white_win : R.string.black_win;
-        if (mAlertDialog == null) {
-            AlertDialog.Builder builder = new AlertDialog.Builder(this);
-            mAlertDialog = builder.setPositiveButton(R.string.quit, new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialog, int which) {
-                    finish();
-                }
-            }).setNegativeButton(R.string.start_again, new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialog, int which) {
-                    mGomokuView.start();
-                    dialog.dismiss();
-                }
-            }).setCancelable(false).create();
-        }
-        mAlertDialog.setMessage(getString(msgId));
-        mAlertDialog.show();
+    public void onMenuItemClick(View clickedView, int position) {
+        Toast.makeText(this, "Clicked on position: " + position, Toast.LENGTH_SHORT).show();
     }
 
     @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.btn_revoke:
-                mGomokuView.revoke();
-                break;
-            case R.id.btn_restart:
-                mGomokuView.start();
-                break;
-        }
+    public void onMenuItemLongClick(View clickedView, int position) {
+        Toast.makeText(this, "Long clicked on position: " + position, Toast.LENGTH_SHORT).show();
     }
+
 }
--- app/src/main/java/com/adrian/gomoku/activity/MainFragment.java
+++ app/src/main/java/com/adrian/gomoku/activity/MainFragment.java
@@ -1,19 +1,85 @@
 package com.adrian.gomoku.activity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.adrian.gomoku.R;
+import com.adrian.gomoku.views.GomokuView;
+
+import de.cketti.library.changelog.ChangeLog;
 
 
-public class MainFragment extends Fragment {
+public class MainFragment extends Fragment implements GomokuView.IGameOverListener, View.OnClickListener {
+
+    private GomokuView mGomokuView;
+    private AlertDialog mAlertDialog;
+    private Button mRevokeBtn;
+    private Button mRestartBtn;
+
+    private long mLastBackPress;
+    private static final long mBackPressThreshold = 3500;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+        mGomokuView = (GomokuView) rootView.findViewById(R.id.gomoku_view);
+        mRevokeBtn = (Button) rootView.findViewById(R.id.btn_revoke);
+        mRestartBtn = (Button) rootView.findViewById(R.id.btn_restart);
+        mGomokuView.setListener(this);
+        mRevokeBtn.setOnClickListener(this);
+        mRestartBtn.setOnClickListener(this);
+
+        ChangeLog cl = new ChangeLog(getContext());
+        if (cl.isFirstRun()) {
+            cl.getLogDialog().show();
+        }
+
+        mGomokuView.setAiOpened(true);
+        if (mGomokuView.isAiOpened()) {
+            mRevokeBtn.setVisibility(View.GONE);
+        } else {
+            mRevokeBtn.setVisibility(View.VISIBLE);
+        }
         return rootView;
+    }
+
+    @Override
+    public void gameOver(boolean isWhiteWin) {
+        int msgId = isWhiteWin ? R.string.white_win : R.string.black_win;
+        if (mAlertDialog == null) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+            mAlertDialog = builder.setPositiveButton(R.string.quit, new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    getActivity().finish();
+                }
+            }).setNegativeButton(R.string.start_again, new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    mGomokuView.start();
+                    dialog.dismiss();
+                }
+            }).setCancelable(false).create();
+        }
+        mAlertDialog.setMessage(getString(msgId));
+        mAlertDialog.show();
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_revoke:
+                mGomokuView.revoke();
+                break;
+            case R.id.btn_restart:
+                mGomokuView.start();
+                break;
+        }
     }
 }
